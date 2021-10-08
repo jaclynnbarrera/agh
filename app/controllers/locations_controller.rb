bfaf9f6ad3f7122910cab98471f037148fe7c113
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-10-08 11:03:13 -0400

    loads local JSON file to be read/updated

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,14 @@
+require 'geocoder'
+require 'httparty'
+
 class LocationsController < ApplicationController
+    file = File.read('public/locations.json')
+    @@data_hash = JSON.parse(file)
+    @@test_location = @@data_hash[0]
+    @@matches = []
+
+    def index
+        binding.pry
+    end
+
 end
